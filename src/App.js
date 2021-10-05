9c089d683c616a4bd959b9ed93858b7a381b040d
David Fletcher <davidfletcher@Davids-Air-2.lan>
2021-10-05 11:49:12 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@
 */
 
 // Import the state hook
-import React from 'react';
+import React, { useState } from 'react';
 // Import the Posts (plural!) and SearchBar components, since they are used inside App component
 // Import the dummyData
 import './App.css';
@@ -33,6 +33,7 @@ const App = () => {
     <div className='App'>
       {/* Add SearchBar and Posts here to render them */}
       {/* Check the implementation of each component, to see what props they require, if any! */}
+      
     </div>
   );
 };
